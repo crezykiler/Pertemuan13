f731c065da5da19d0ca09aeeac1e45a8441aa4f6
zaki setyawan <kiminsetyawan@gmail.com>
2024-12-02 08:30:17 +0700

    modif no 3

--- hitungTotalHarga17.java
+++ hitungTotalHarga17.java
@@ -12,7 +12,7 @@ public class hitungTotalHarga17 {
         } else if (kodePromo.equals("DISKON30")) {
             hargaTotal *= 0.7; // Diskon 30%
             System.out.println("Kode promo DISKON30 berlaku. Anda mendapat diskon 30%.");
-        } else {
+        } else if (!kodePromo.isEmpty()) {
             System.out.println("Kode promo invalid! Tidak ada diskon yang diberikan.");
         }
 
@@ -21,17 +21,25 @@ public class hitungTotalHarga17 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int totalKeseluruhan = 0;
 
-        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
-        int pilihanMenu = sc.nextInt();
-        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
-        int banyakItem = sc.nextInt();
-        sc.nextLine(); 
         System.out.print("Masukkan kode promo (jika ada): ");
         String kodePromo = sc.nextLine();
 
-        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem, kodePromo);
+        while (true) {
+            System.out.print("\nMasukkan nomor menu yang ingin Anda pesan (1-6) atau 0 untuk selesai: ");
+            int pilihanMenu = sc.nextInt();
+            if (pilihanMenu == 0) {
+                break;
+            }
 
-        System.out.print("Total harga untuk pesanan Anda: Rp " + totalHarga);
+            System.out.print("Masukkan jumlah item untuk menu ini: ");
+            int banyakItem = sc.nextInt();
+
+            int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem, kodePromo);
+            totalKeseluruhan += totalHarga;
+        }
+
+        System.out.println("\nTotal keseluruhan pesanan Anda: Rp " + totalKeseluruhan);
     }
 }
