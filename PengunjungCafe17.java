afb4239ac23c0632f4b7ff47b0a5bc78ce82a5cd
zaki setyawan <kiminsetyawan@gmail.com>
2024-12-02 09:58:06 +0700

    sip

--- PengunjungCafe17.java
+++ PengunjungCafe17.java
@@ -1,13 +1,21 @@
 public class PengunjungCafe17 {
 
-    public static void daftarPengunjung(String... namaPengunjung) {
-        System.out.println("Daftar Nama Pengunjung:");
-        for (String pengunjung : namaPengunjung) {
-            System.out.println("- " + pengunjung);
+    public static void daftarNama(String... nama) {
+        System.out.println("Daftar Nama:");
+        for (String n : nama) {
+            System.out.println(n);
+        }
+    }
+  
+    public static void daftarAngka(int... angka) {
+        System.out.println("Daftar Angka:");
+        for (int a : angka) {
+            System.out.println(a);
         }
     }
 
     public static void main(String[] args) {
-        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarNama("Ali", "Budi", "Citra");
+        daftarAngka(1, 2, 3, 4);
     }
 }
